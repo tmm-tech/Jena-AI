149a90ef2437ceb48cdad90d469e5615bc1677ab
tmm-tech <tonymugi074@gmail.com>
2024-07-07 15:03:08 +0300

    Jena Setup

--- jena_assistant/jena/models.py
+++ jena_assistant/jena/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
 class Task(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
     completed = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.title
+    
--- jena_assistant/jena/views.py
+++ jena_assistant/jena/views.py
@@ -5,7 +5,3 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Welcome to Jena Backend")
-
-class TaskViewSet(viewsets.ModelViewSet):
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializer
